579a46c4678de76f1877e9d25c494112132fe7de
gnana-19 <47478567+gnana-19@users.noreply.github.com>
2020-10-26 23:19:18 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
 // https://atcoder.jp/posts/518
@@ -17,8 +17,8 @@ using namespace std;
 // using namespace atcoder;
 
 using ld = long double;
-#define int long long
-# define mod 1000000007 
+using ll = long long;
+
 #define F first
 #define S second
 #define s(x) set<x>
@@ -31,6 +31,7 @@ using ld = long double;
 
 #define eb emplace_back
 #define mp make_pair
+#define pb push_back
 #define bs binary_search
 #define lb lower_bound
 #define ub upper_bound
@@ -39,26 +40,7 @@ using ld = long double;
 #define ss(v) stable_sort(all(v))
 #define fori(i, a, b) for (int i = a; i <= b; i++)
 #define rofi(i, a, b) for (int i = a; i >= b; --i)
-#define scn(v1) for(auto &x : v1) cin >> x;
-#define deb(x) cout << #x << "=" << x << endl;
 #define R(x) reverse(all(x));
-
-#define FILE_READ_IN freopen("input.txt", "r", stdin);
-#define FILE_READ_OUT freopen("output.txt", "w", stdout);
-
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define stprs(x) cout << fixed << setprecision(x);
-
-int power(int a, int b, int m = mod)
-{if(b==0)return 1;
- else if(b==1)return a%m;
- else{ll t=power(a,b>>1,m);
-      t=(t * t)%m;
-      if(b&1)
-          return (t * a)%m;
-      else 
-          return t;
-     }
 
 const char nl = '\n';
 const char sp = ' ';
@@ -66,83 +48,80 @@ const int mod1 = 1e9 + 7;
 const int mod2 = 998244353;
 const ld PI = acos(-1);
 
-#define debug cerr
-#define name(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define stprs(x) cout << fixed << setprecision(x);
 
-template<class A>
-ostream& operator << (ostream& out, const set<A> &a) {
-    out << "[";
-    for (auto it = a.begin(); it != a.end(); ++it) {
-        if (it != a.begin())
-            out << ", ";
-        out << *it;
+// Taken from errichto's submissions.
+// https://codeforces.com/contest/1408/submission/94342467
+#define sim template <class c
+#define ris return *this
+#define dor > debug &operator<<
+#define eni(x)                                                                    \
+    sim > typename enable_if<sizeof dud<c>(0) x 1, debug &>::type operator<<(c i) \
+    {
+sim > struct rge
+{
+    c b, e;
+};
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c *x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug
+{
+#ifdef LOCAL
+    ~debug()
+    {
+        cerr << endl;
     }
-    return out << "]";
+    eni(!=) cerr << boolalpha << i;
+    ris;
+} eni(==) ris << range(begin(i), end(i));
 }
-
-template<class A>
-ostream& operator << (ostream& out, const vector<A> &a) {
-    // out << "[";
-    for (auto it = a.begin(); it != a.end(); ++it) {
-        if (it != a.begin())
-            // out << ", ";
-            out << ' ';
-        out << *it;
+sim, class b dor(pair<b, c> d)
+{
+    ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d)
+{
+    *this << "[";
+    for (auto it = d.b; it != d.e; ++it)
+        *this << ", " + 2 * (it == d.b) << *it;
+    ris << "]";
+}
+#else
+    sim dor(const c &)
+    {
+        ris;
     }
-    return out;
-    return out << "]";
+#endif
 }
-
-template<class A, class B>
-ostream& operator << (ostream& out, const pair<A, B> &a) {
-    return out <<  "(" << a.first << ", " << a.second << ")";
-}
-
-template<class A, class B>
-ostream& operator << (ostream& out, const map<A, B> &a) {
-    out << "[";
-    for (auto it = a.begin(); it != a.end(); ++it) {
-        if (it != a.begin()) {
-            out << ", ";
-        }
-        out << *it;
-    }
-    return out << "]";
-}
-template<class A, class B>
-ostream& operator << (ostream& out, const unordered_map<A, B> &a) {
-    out << "[";
-    for (auto it = a.begin(); it != a.end(); ++it) {
-        if (it != a.begin()) {
-            out << ", ";
-        }
-        out << *it;
-    }
-    return out << "]";
-}
-
-template<typename... T>
-void read(T&... args) {
-    ((cin >> args), ...);
-}
-
-template<typename... T>
-void print(T... args) {
-    ((cout << args), ...);
-}
+;
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 
 // variables and consts
+// const int mod = mod1;
+// const int mod = mod2;
+// using mint = static_modint<mod>;
 // -- variables and consts --
 
-void solve(void) {
-    
+void solve()
+{
 }
 
-int32_t main(void) {
-    fastio
-    int __ = 1;
-    // read(__);
-    while (__--) {
+int32_t main()
+{
+    int T = 1;
+    // fastio
+    // stprs(10);
+    // cin >> T;
+    // scanf("%d", &T);
+    for (int test = 1; test <= T; test++)
+    {
+        // cout << "Case " << test << ": ";
+        // cout << "Case #" << test << ": ";
+        // cout << nl;
+        // printf("Case %d: ", test);
+        // printf("Case #%d: ", test);
         solve();
     }
     return 0;
